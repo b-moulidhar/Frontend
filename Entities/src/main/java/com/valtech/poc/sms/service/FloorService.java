6a3d2219df6a987cedfdc8100232c1858dbee739
Moyukh Saha <moyukh.saha@valtech.com>
2023-04-04 17:03:47 +0530

    seat updation

--- Entities/src/main/java/com/valtech/poc/sms/service/FloorService.java
+++ Entities/src/main/java/com/valtech/poc/sms/service/FloorService.java
@@ -5,15 +5,23 @@ import java.util.List;
 import com.valtech.poc.sms.entities.Floors;
 
 public interface FloorService {
-	
+
+//	public void addFloorSeats(Floors floor);
+//
+//	public void updateFloorSeats(Floors floor);
+//
+//	public void deleteFloorSeats(int floorId);
+
+//	---------------------------------------------------------------------------------------------------------
+
 	public List<Floors> getAllFloors();
 
-	public Floors getFloorById(int fId);
+	public Floors getFloorById(int f_id);
 
-	public void addFloorSeats(int fId, int seatsToAdd);
+	public void addFloorSeats(int f_id, int seatsToAdd);
 
-	public void deleteFloorSeats(int fId, int seatsToDelete);
-
-	public void updateFloorSeats(int fId, int newSeats);
+	public void deleteFloorSeats(int f_id, int seatsToDelete);
+	
+	public void updateFloorSeats(int f_id, int updatedNumberOfSeats);
 
 }
