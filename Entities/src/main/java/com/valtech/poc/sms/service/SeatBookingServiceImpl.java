6818ef255a6f5719e5f6b13f35d32db941ba890b
Shreya CN <shreya.cn@valtech.com>
2023-04-06 12:05:35 +0530

    minor changes

--- Entities/src/main/java/com/valtech/poc/sms/service/SeatBookingServiceImpl.java
+++ Entities/src/main/java/com/valtech/poc/sms/service/SeatBookingServiceImpl.java
@@ -89,12 +89,12 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 		List<SeatsBooked> sb = seatBookingDao.findCurrentSeat(emp);
 
 		Collections.sort(sb, new Comparator<SeatsBooked>() {
-		      @Override
-		      public int compare(SeatsBooked o1, SeatsBooked o2) {
-		        return o1.getSbDate().compareTo(o2.getSbDate());
-		      }
-		    });
-		
+			@Override
+			public int compare(SeatsBooked o1, SeatsBooked o2) {
+				return o1.getSbDate().compareTo(o2.getSbDate());
+			}
+		});
+
 		SeatsBooked latestSb = sb.get(0);
 		return latestSb;
 	}
@@ -146,28 +146,26 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
-
-    @Override
-    public List<SeatsBooked> getSeatsBookedByEmployeeAndDate(int empId, LocalDateTime startDate, LocalDateTime endDate) {
-        return seatBookingDao.getSeatsBookedByEmployeeAndDate(empId, startDate, endDate);
-    }
-
-    @Override
-    public List<SeatsBooked> getSeatsBookedByDate(LocalDateTime startDate, LocalDateTime endDate) {
-        return seatBookingDao.getSeatsBookedByDate(startDate, endDate);
-    }
-    
-    @Override
-    public List<SeatsBooked> getSeatsBookedByShiftTimingBetweenDates(int stId,LocalDateTime startDate, LocalDateTime endDate) {
-        return seatBookingDao.getSeatsBookedByShiftTimingBetweenDates(stId, startDate, endDate);
-    }
-    
 
 	@Override
-	public boolean checkIftheSeatIsCurrentlyBooked(int eId,  LocalDateTime fromDateTime,
-			LocalDateTime toDateTime) {
+	public List<SeatsBooked> getSeatsBookedByEmployeeAndDate(int empId, LocalDateTime startDate,
+			LocalDateTime endDate) {
+		return seatBookingDao.getSeatsBookedByEmployeeAndDate(empId, startDate, endDate);
+	}
+
+	@Override
+	public List<SeatsBooked> getSeatsBookedByDate(LocalDateTime startDate, LocalDateTime endDate) {
+		return seatBookingDao.getSeatsBookedByDate(startDate, endDate);
+	}
+
+	@Override
+	public List<SeatsBooked> getSeatsBookedByShiftTimingBetweenDates(int stId, LocalDateTime startDate,
+			LocalDateTime endDate) {
+		return seatBookingDao.getSeatsBookedByShiftTimingBetweenDates(stId, startDate, endDate);
+	}
+
+	@Override
+	public boolean checkIftheSeatIsCurrentlyBooked(int eId, LocalDateTime fromDateTime, LocalDateTime toDateTime) {
 		// TODO Auto-generated method stub
 		return seatBookingDao.checkIfEmployeeAlreadyBookTheSeat(eId, fromDateTime, toDateTime);
 	}
@@ -175,7 +173,7 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 	@Override
 	public boolean checkIftheSeatIsCurrentlyBookedDaily(int eId, LocalDateTime fromDateTime) {
 		// TODO Auto-generated method stub
-		return seatBookingDao.checkIfEmployeeAlreadyBookTheSeatDaily(eId,fromDateTime);
+		return seatBookingDao.checkIfEmployeeAlreadyBookTheSeatDaily(eId, fromDateTime);
 	}
 
 	@Override
@@ -191,48 +189,16 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 		if (checkIftheSeatIsCurrentlyBookedDaily(eId, fromDateTime)) {
 			return "This employee has already booked the seat.";
 		} else {
-
-			if(CalendarUtil.isDateDisabled(fromDate)) {
-			System.out.println("The date falls on sunday or saturday");
-			}
-			else if (holidayService.isHoliday(fromDate)) {
-		    System.out.println("Seat Booking not allowed on holidays");
-		}
-			else {
-			String code = adminService.generateQrCode(eId);
-			LocalDateTime localDateTime = fromDate.atStartOfDay();
-			//LocalDateTime dateTime = LocalDateTime.parse(formatter.format(localDateTime), formatter);
-			long limit = 60000*240;
-			// check for recurring seats
-			if (CheckIfTheSameSeatBookingRecurring(eId)) {
-				Seat recSeat = getSeatById(sId);
-
-				//SeatsBooked sb = new SeatsBooked( localDateTime, null, null, true, code, recSeat, emp, false, false, false, null);
-
-				SeatsBooked sb = new SeatsBooked(localDateTime, null, null, true, code, recSeat, emp, false, false,false,st);
-
-				SeatsBooked savedSeatsBooked = saveSeatsBookedDetails(sb);
-				scheduledTask.scheduleTask(limit, savedSeatsBooked);
-				//mailContent.dailyNotification(emp);
-				return "The Same Seat is booked successfully because you are selecting this seat more than 3 times with ID: "
-						+ savedSeatsBooked.getSbId();
-			} else {
-
-				//SeatsBooked sb = new SeatsBooked( localDateTime, null, null, true, code, seat, emp, false, false, false, null);
-
-				SeatsBooked sb = new SeatsBooked(localDateTime, null, null, true, code, seat, emp, false, false,false,st);
-
-
 			if (CalendarUtil.isDateDisabled(fromDate)) {
 				System.out.println("The date falls on sunday or saturday");
 			} else if (holidayService.isHoliday(fromDate)) {
 				System.out.println("Seat Booking not allowed on holidays");
 			} else {
-				String code1 = adminService.generateQrCode(eId);
-				LocalDateTime localDateTime1 = fromDate.atStartOfDay();
+				String code = adminService.generateQrCode(eId);
+				LocalDateTime localDateTime = fromDate.atStartOfDay();
 				// LocalDateTime dateTime = LocalDateTime.parse(formatter.format(localDateTime),
 				// formatter);
-				long limit1 = 60000 * 240;
+				long limit = 60000 * 240;
 				// check for recurring seats
 //			if (CheckIfTheSameSeatBookingRecurring(eId)) {
 //				Seat recSeat = getSeatById(sId);
@@ -243,18 +209,13 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 //				return "The Same Seat is booked successfully because you are selecting this seat more than 3 times with ID: "
 //						+ savedSeatsBooked.getSbId();
 //			} else {
-			//	SeatsBooked sb = new SeatsBooked(localDateTime, null, null, true, code, seat, emp, false, false, true,
-				//		st);
+				SeatsBooked sb = new SeatsBooked(localDateTime, null, null, true, code, seat, emp, false, false, true,
+						st);
 				SeatsBooked savedSeatsBooked = saveSeatsBookedDetails(sb);
 				if(sb.isFood()==true) {
 					seatBookingDao.updatFoodCount(sb.getSbDate());
 				}
 				scheduledTask.scheduleTask(limit, savedSeatsBooked);
-//				SeatsBooked sb1 = new SeatsBooked(localDateTime1, null, null, true, code1, seat, emp, false, false, false,
-//						st);
-//
-//				SeatsBooked savedSeatsBooked = saveSeatsBookedDetails(sb1);
-				scheduledTask.scheduleTask(limit1, savedSeatsBooked);
 //				mailContent.dailyNotification(emp);
 				// check if employee is booking a seat again on the same day
 //				if (canEmployeeBookSeat(eId, sId,fromDateTime)) {
@@ -263,15 +224,11 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 				return "Seats booked created successfully with ID: " + savedSeatsBooked.getSbId();
 				// }
 			}
-			//return "Seat Booked Succesfully";
-			}
-		
-			}
-			}
-		return "Seat Booked Succesfully";
+			return "Seat Booked Succesfully";
+
 		}
-			
-	
+
+	}
 
 	@Override
 	public String createSeatsBookedWeekly(int eId, int sId, int stId, String from, String to) {
@@ -302,24 +259,10 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 						seatBookingDao.updatFoodCount(sb.getSbDate());
 					}
 				}
-
-					 {
-			LocalDateTime localDateTime = date.atStartOfDay();
-			String code = adminService.generateQrCode(eId);
-
-			//SeatsBooked sb = new SeatsBooked( localDateTime, null, null, true, code, seat, emp, false, false, false, null);
-
-			SeatsBooked sb = new SeatsBooked(localDateTime, null, null, true, code, seat, emp, false, false,false,st);
-
-			seatsBookedRepo.save(sb);
 			}
-			}
-			return "Seats booked successfully";
-			
+			return "Seats booked successfully ";
 		}
-		
 	}
-
 	@Override
 	public byte[] generateSeatsBookedReportPDF(LocalDateTime startDate, LocalDateTime endDate) throws Exception {
 		List<SeatsBooked> seatsBooked = getSeatsBookedByDate(startDate, endDate);
@@ -327,40 +270,40 @@ public class SeatBookingServiceImpl implements SeatBookingService {
 		byte[] pdf = seatBookingDao.generateSeatsBookedPDF(seatsBooked);
 		return pdf;
 	}
-	@Override
-	public byte[] generateSeatsBookedByEmployeeReportPDF(int empId,LocalDateTime startDate, LocalDateTime endDate) throws Exception {
-	    List<SeatsBooked> seatsBooked = getSeatsBookedByEmployeeAndDate(empId, startDate, endDate);
-	    
-	    byte[] pdf=seatBookingDao.generateSeatsBookedPDF(seatsBooked);
-	    return pdf;
-	}
-	@Override
-	public byte[] generateSeatsBookedByShiftReportPDF(int stId,LocalDateTime startDate, LocalDateTime endDate) throws Exception {
-	    List<SeatsBooked> seatsBooked = getSeatsBookedByShiftTimingBetweenDates(stId, startDate, endDate);
-	    
-	    byte[] pdf=seatBookingDao.generateSeatsBookedPDF(seatsBooked);
-	    return pdf;
-	}
-
 
 	@Override
-	public List<Map<String,Object>> GettingDetailsOfViwPass(int eid) {
+	public byte[] generateSeatsBookedByEmployeeReportPDF(int empId, LocalDateTime startDate, LocalDateTime endDate)
+			throws Exception {
+		List<SeatsBooked> seatsBooked = getSeatsBookedByEmployeeAndDate(empId, startDate, endDate);
+
+		byte[] pdf = seatBookingDao.generateSeatsBookedPDF(seatsBooked);
+		return pdf;
+	}
+
+	@Override
+	public byte[] generateSeatsBookedByShiftReportPDF(int stId, LocalDateTime startDate, LocalDateTime endDate)
+			throws Exception {
+		List<SeatsBooked> seatsBooked = getSeatsBookedByShiftTimingBetweenDates(stId, startDate, endDate);
+
+		byte[] pdf = seatBookingDao.generateSeatsBookedPDF(seatsBooked);
+		return pdf;
+	}
+
+	@Override
+	public List<Map<String, Object>> GettingDetailsOfViwPass(int eid) {
 		// TODO Auto-generated method stub
 		return seatBookingDao.GettingDetailsOfViwPass(eid);
 	}
 
+	@Override
+	public List<Seat> getTopFivePopularSeats() {
+		return seatBookingDao.getTopFivePopularSeats();
+	}
 
-	
-	 @Override
-	   public List<Seat> getTopFivePopularSeats() {
-	        return seatBookingDao.getTopFivePopularSeats();
-	    }
-	
 //    @Override
 //    public List<Object[]> getTopFivePopularSeats() {
 //        return seatRepo.findTopFivePopularSeats();
 //
 //}
-
 
 }
